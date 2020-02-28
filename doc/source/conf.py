2a03af6641816997d7df8e8911c886a5f3921088
Jolando Kisse <44288328+jkisse@users.noreply.github.com>
2020-02-28 16:39:03 +0100

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,10 +13,10 @@
 import os
 import sys
 
-sys.path.append(0, os.path.abspath('../..'))
-sys.path.append(0, os.path.abspath('..'))
-sys.path.append(0, os.path.abspath('../../..'))
-sys.path.append(0, os.path.abspath('../../../pandapower'))
+sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath('../..'))
+sys.path.append(os.path.abspath('../../..'))
+sys.path.append(os.path.abspath('../../../pandapower'))
 
 
 master_doc = 'index'
