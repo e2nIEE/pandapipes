fda228235638702f155bfcaceba3ad7ad1327840
Jolando Kisse <jolando.kisse@uni-kassel.de>
2020-03-18 19:41:53 +0100

    test init imports re-ordered
    Signed-off-by: Jolando Kisse <jolando.kisse@uni-kassel.de>

--- pandapipes/test/__init__.py
+++ pandapipes/test/__init__.py
@@ -2,9 +2,9 @@
 # and Energy System Technology (IEE), Kassel. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-from pandapipes.test.run_tests import *
-from pandapipes.test.test_imports import *
-from pandapipes.test.test_toolbox import *
 import os
 from pandapipes import pp_dir
 test_path = os.path.join(pp_dir, 'test')
+from pandapipes.test.run_tests import *
+from pandapipes.test.test_imports import *
+from pandapipes.test.test_toolbox import *
