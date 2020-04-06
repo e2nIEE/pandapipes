65ead83ad03e1ff948d2dfdefddef8b9760411e3
Tabea Trummel <tabea.trummel@iee.fraunhofer.de>
2020-04-06 10:58:14 +0200

    - heat test networks created in OpenModelica and added as json to simple_test_networks - simple_heat_transfer_networks created - pipeflow_openmodelica_comparison adjusted

--- pandapipes/test/openmodelica_comparison/pipeflow_openmodelica_comparison.py
+++ pandapipes/test/openmodelica_comparison/pipeflow_openmodelica_comparison.py
@@ -5,8 +5,10 @@
 import numpy as np
 import pandapipes as pp
 import pandas as pd
+import statistics as st
 from pandapipes.plotting import simple_plot
 from pandapipes.properties.fluids import get_fluid
+from pandapipes.component_models import Pipe
 
 try:
     import pplog as logging
@@ -17,14 +19,34 @@ logger = logging.getLogger(__name__)
 
 
 def pipeflow_openmodelica_comparison(net, log_results=True, friction_model='colebrook',
-                                     only_update_hydraulic_matrix=False):
-    pp.pipeflow(
-        net, stop_condition="tol", iter=100, tol_p=1e-7, tol_v=1e-7, friction_model=friction_model,
-        only_update_hydraulic_matrix=only_update_hydraulic_matrix)
+                                     mode='normal', only_update_hydraulic_matrix=False):
+    """
+        Comparison of the calculations of OpenModelica and pandapipes.
+
+        :param net: converted OpenModelica network
+        :type net: pandapipesNet
+        :param log_results:
+        :type log_results: bool, True
+        :param friction_model:
+        :type friction_model: str, "colebrook"
+        :param mode:
+        :type mode: str, "nomral"
+        :param only_update_hydraulic_matrix:
+        :type only_update_hydraulic_matrix: bool, False
+        :return: p_diff, v_diff_abs
+        :rtype: one-dimensional ndarray with axis labels
+    """
+    pp.pipeflow(net, stop_condition="tol", iter=100, tol_p=1e-7, tol_v=1e-7, friction_model=friction_model,
+                mode=mode, only_update_hydraulic_matrix=only_update_hydraulic_matrix)
 
     p_om = net.junction.p_om
     p_valid = pd.notnull(p_om)
     p_om = p_om.loc[p_valid]
+
+    T_diff_abs_pipe = pd.Series()
+    T_om_pipe = pd.Series()
+    T_mean_pandapipes_pipe = pd.Series()
+    T_diff_mean_pipe = pd.Series()
 
     if get_fluid(net).is_gas:
         if 'pipe' in net:
@@ -60,6 +82,10 @@ def pipeflow_openmodelica_comparison(net, log_results=True, friction_model='cole
         if 'pipe' in net:
             v_diff_mean_pipe, v_diff_abs_pipe, v_mean_pandapipes_pipe, v_om_pipe = \
                 retrieve_velocity_liquid(net, element="pipe")
+
+            if mode == "all" or mode == "heat":
+                T_diff_mean_pipe, T_diff_abs_pipe, T_mean_pandapipes_pipe, T_om_pipe = \
+                    retrieve_temperature_liquid(net)
         else:
             v_diff_abs_pipe = pd.Series()
             v_om_pipe = pd.Series()
@@ -67,7 +93,7 @@ def pipeflow_openmodelica_comparison(net, log_results=True, friction_model='cole
             v_diff_mean_pipe = pd.Series()
 
         if 'valve' in net:
-            v_diff_mean_valve, v_diff_abs_valve, v_mean_pandapipes_valve, v_om_pipe = \
+            v_diff_mean_valve, v_diff_abs_valve, v_mean_pandapipes_valve, v_om_valve = \
                 retrieve_velocity_liquid(net, element="valve")
         else:
             v_diff_abs_valve = pd.Series()
@@ -98,21 +124,33 @@ def pipeflow_openmodelica_comparison(net, log_results=True, friction_model='cole
     '''
 
     if log_results:
-        logger.info("p_OM %s" % p_om)
+        logger.info("p_om %s" % p_om)
         logger.info("p_PP %s" % p_pandapipes)
-        logger.info("v_OM_pipe %s" % v_om_pipe)
+        logger.info("v_om_pipe %s" % v_om_pipe)
         logger.info("v_PP_valve %s" % v_om_valve)
         logger.info("v_PP_pipe %s" % v_mean_pandapipes_pipe)
         logger.info("v_PP_valve %s" % v_mean_pandapipes_valve)
 
-        logger.info("Druckdifferenz: %s" % p_diff)
-        logger.info("Geschwindigkeitsdifferenz Rohr: \n %s" % diff_results_v_pipe)
-        logger.info("Geschwindigkeitsdifferenz Ventil: \n %s" % diff_results_v_valve)
+        logger.info("pressure difference: %s" % p_diff)
+        logger.info("velocity difference pipe: \n %s" % diff_results_v_pipe)
+        logger.info("velocity difference valve: \n %s" % diff_results_v_valve)
 
-    return p_diff, v_diff_abs
+    return p_diff, v_diff_abs, T_diff_mean_pipe
 
 
 def retrieve_velocity_liquid(net, element="pipe"):
+    """
+        Get the calculated velocities for a liquid fluid in pandapipes and OpenModelica and
+        calculate the absolute and relative errors.
+
+        :param net: converted OpenModelica network
+        :type net: pandapipesNet
+        :param element: either pipe or valve
+        :type element: str, default "pipe"
+        :return: relative and absolute error (v_diff_mean, v_diff_abs) of average velocities,
+        calculated velocities from pandapipes and OpenModelica (v_mean_pandapipes, v_om)
+        :rtype: one-dimensional ndarray with axis labels
+    """
     if 'v_om' not in net[element]:
         net[element]['v_om'] = []
     v_om = net[element].v_om
@@ -141,6 +179,18 @@ def retrieve_velocity_liquid(net, element="pipe"):
 
 
 def retrieve_velocity_gas(net, element='pipe'):
+    """
+        Get the calculated velocities for a gaseous fluid in pandapipes and OpenModelica and
+        calculate the absolute and relative errors.
+
+        :param net: converted OpenModelica network
+        :type net: pandapipesNet
+        :param element: either pipe or valve
+        :type element: str, default "pipe"
+        :return: relative and absolute error (v_diff_mean, v_diff_abs) of average velocities,
+        calculated velocities from pandapipes and OpenModelica (v_mean_pandapipes, v_om)
+        :rtype: one-dimensional ndarray with axis labels
+    """
     if 'v_om' not in net[element]:
         net[element]['v_om'] = []
     v_om = net[element].v_om
@@ -169,3 +219,38 @@ def retrieve_velocity_gas(net, element='pipe'):
     v_om = pd.Series(v_om, range(len(v_om)))
 
     return v_diff_from, v_diff_to, v_diff_mean, v_diff_abs, v_mean_pandapipes, v_om
+
+
+def retrieve_temperature_liquid(net):
+    """
+        Get the calculated temperatures for a liquid fluid in pandapipes and OpenModelica and
+        calculate the absolute and relative errors.
+
+        :param net: converted OpenModelica network
+        :type net: pandapipesNet
+        :return: relative and absolute error (T_diff_mean, T_diff_abs) of average velocities,
+        calculated velocities from pandapipes and OpenModelica (T_mean_pandapipes, T_om)
+        :rtype: one-dimensional ndarray with axis labels
+    """
+    T_om = net.pipe["T_om"]
+    num_of_pipes = len(T_om)
+    T_mean_om = np.arange(num_of_pipes)
+    T_mean_pandapipes = np.arange(num_of_pipes)
+
+    for i in range(num_of_pipes):
+        T_mean_om[i] = st.mean(T_om[i])
+
+    for j in range(num_of_pipes):
+        pipe_res = Pipe.get_internal_results(net,[j])
+        T_mean_pandapipes[j] = st.mean(pipe_res["TINIT"][:,1])
+
+    T_diff_mean = np.abs(1 - T_mean_pandapipes / T_mean_om)
+    T_diff_abs = np.abs(T_mean_om - T_mean_pandapipes)
+
+    T_diff_mean = pd.Series(T_diff_mean, range(len(T_diff_mean)))
+    T_diff_abs = pd.Series(T_diff_abs, range(len(T_diff_abs)))
+
+    T_om = pd.Series(T_om, range(len(T_om)))
+    T_mean_pandapipes = pd.Series(T_mean_pandapipes, range(len(T_mean_pandapipes)))
+
+    return T_diff_mean, T_diff_abs, T_mean_pandapipes, T_om
