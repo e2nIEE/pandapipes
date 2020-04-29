bc2d616b91641ca575c272d6f6714396e99dab1f
dlohmeier <daniel.lohmeier@iee.fraunhofer.de>
2020-04-29 08:53:00 +0200

    a lot of new toolbox functions; moved sum_by_group to internals_toolbox

--- pandapipes/component_models/ext_grid_component.py
+++ pandapipes/component_models/ext_grid_component.py
@@ -12,7 +12,7 @@ from pandapipes.idx_node import PINIT, LOAD, TINIT, NODE_TYPE, NODE_TYPE_T, P, T
 from pandapipes.idx_branch import FROM_NODE, TO_NODE, LOAD_VEC_NODES
 
 from pandapipes.pipeflow_setup import get_lookup
-from pandapipes.toolbox import _sum_by_group
+from pandapipes.internals_toolbox import _sum_by_group
 
 try:
     import pplog as logging
