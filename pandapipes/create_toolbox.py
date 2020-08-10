e044b085c89ce3defbe076e9a2ec09e8a17728ba
sdrauz <Simon.Ruben.Drauz@iee.fraunhofer.de>
2020-08-10 14:42:09 +0200

    small bugfix

--- pandapipes/create_toolbox.py
+++ pandapipes/create_toolbox.py
@@ -8,7 +8,7 @@ from pandapipes.constants import GRAVITATION_CONSTANT, P_CONVERSION
 
 def transform_water_tower_into_ext_grid(net, junction, height_m, t_k=293.15, name='water_tower', in_service=True,
                                        index=None, type='pt'):
-    density = net.fluid.get_density(t_k).item()
+    density = net.fluid.get_density([t_k])
 
     p_bar = density * height_m * GRAVITATION_CONSTANT / P_CONVERSION
 
