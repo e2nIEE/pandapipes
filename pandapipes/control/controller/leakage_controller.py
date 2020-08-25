1d4c21e620c5b003fe3b0d7a2e9f9d1519e80e1f
Tabea Trummel <tabea.trummel@iee.fraunhofer.de>
2020-08-25 11:46:25 +0200

    - loops as far as possible, except one, because here create_sink must be called - corrections made

--- pandapipes/control/controller/leakage_controller.py
+++ pandapipes/control/controller/leakage_controller.py
@@ -45,7 +45,7 @@ class LeakageController(Controller):
 
     def __init__(self, net, element, element_index, output_area_m2, profile_name=None,
                  scale_factor=1.0, in_service=True, recycle=True, order=0, level=0,
-                 drop_same_existing_ctrl=False, set_q_from_cosphi=False, matching_params=None, initial_pipeflow=False,
+                 drop_same_existing_ctrl=False, matching_params=None, initial_run=True,
                  **kwargs):
 
         if element not in ["pipe", "valve", "heat_exchanger"]:
@@ -57,7 +57,7 @@ class LeakageController(Controller):
         # just calling init of the parent
         super().__init__(net, in_service=in_service, recycle=recycle, order=order, level=level,
                          drop_same_existing_ctrl=drop_same_existing_ctrl,
-                         matching_params=matching_params, initial_powerflow=initial_pipeflow,
+                         matching_params=matching_params, initial_run=initial_run,
                          **kwargs)
 
         self.matching_params = {"element": element, "element_index": element_index}
@@ -71,57 +71,43 @@ class LeakageController(Controller):
         self.values = None
         self.profile_name = profile_name
         self.scale_factor = scale_factor
-        self.initial_pipeflow = initial_pipeflow
+        self.initial_run = initial_run
         self.kwargs = kwargs
-        self.rho_kg_per_m3 = []  # densities for the calculation of leakage mass flows
-        self.v_m_per_s = []  # current flow velocities at pipes
+        self.rho_kg_per_m3 = numpy.array([])  # densities for the calculation of leakage mass flows
+        self.v_m_per_s = numpy.array([])  # current flow velocities at pipes
         self.mass_flow_kg_per_s_init = []  # initial/ previous leakage mass flows
         self.mass_flow_kg_per_s = []  # current leakage mass flows
         self.leakage_index = []  # index of the sinks for leakages
 
-        if set_q_from_cosphi:
-            logger.error("Parameter set_q_from_cosphi deprecated!")
-            raise ValueError
-
     def initialize_control(self):
         """
-        First calculation of a pipeflow without leakage. \n
-        Then define the initial values and create the sinks representing the leaks.
+        Define the initial values and create the sinks representing the leaks.
         """
-        pp.pipeflow(self.net, self.kwargs)
-
         for i in range(len(self.element_index)):
-            self.init_values(self.element_index[i])
-
             index = pp.create_sink(self.net, self.net[self.element].loc[self.element_index[i], "to_junction"],
-                                   mdot_kg_per_s=0, name="leakage"+str(i))
+                                   mdot_kg_per_s=0, name="leakage" + str(i))
             self.leakage_index.append(index)
             self.mass_flow_kg_per_s_init.append(0)
 
-    def init_values(self, index):
+    def init_values(self):
         """
         Initialize the flow velocity and density for the individual control steps.
         """
-        self.v_m_per_s.append(self.net["res_"+self.element].loc[index, "v_mean_m_per_s"])
+        self.v_m_per_s = numpy.array(self.net["res_" + self.element].loc[self.element_index, "v_mean_m_per_s"])
 
-        temp_1 = self.net.res_junction.loc[self.net[self.element].loc[index, "from_junction"], "t_k"]
-        temp_2 = self.net.res_junction.loc[self.net[self.element].loc[index, "to_junction"], "t_k"]
-        self.rho_kg_per_m3.append((self.net.fluid.get_density(temp_1) + self.net.fluid.get_density(temp_2)) / 2)
+        temp_1 = self.net.res_junction.loc[self.net[self.element].loc[self.element_index, "from_junction"], "t_k"]
+        temp_2 = self.net.res_junction.loc[self.net[self.element].loc[self.element_index, "to_junction"], "t_k"]
+        self.rho_kg_per_m3 = (self.net.fluid.get_density(temp_1) + self.net.fluid.get_density(temp_2)) / 2
 
     def is_converged(self):
         """
-        Convergence Condition: Difference between mass flows smaller than 1e-5 = 0.00001 \n
-        Delete the sinks that represented the leaks.
+        Convergence Condition: Difference between mass flows smaller than 1e-5 = 0.00001
         """
-        if not self.mass_flow_kg_per_s:
+        if numpy.array(self.mass_flow_kg_per_s).size == 0:
             return False
 
-        for i in range(len(self.element_index)):
-            if abs(self.mass_flow_kg_per_s_init[i] - self.mass_flow_kg_per_s[i]) > 1e-5:
-                return False
-
-        self.net.sink = self.net.sink.drop(labels=self.leakage_index)
-        self.net.res_sink = self.net.res_sink.drop(labels=self.leakage_index)
+        if abs(numpy.subtract(self.mass_flow_kg_per_s, self.mass_flow_kg_per_s_init)).all() > 1e-5:
+            return False
 
         return True
 
@@ -131,14 +117,20 @@ class LeakageController(Controller):
         """
         pp.pipeflow(self.net, self.kwargs)
         self.mass_flow_kg_per_s = []
-        self.v_m_per_s = []
-        self.rho_kg_per_m3 = []
+        self.v_m_per_s = numpy.array([])
+        self.rho_kg_per_m3 = numpy.array([])
 
-        for i in range(len(self.element_index)):
-            self.init_values(self.element_index[i])
+        self.init_values()
 
-            self.net.sink.loc[self.leakage_index[i], "mdot_kg_per_s"] = self.rho_kg_per_m3[i] * self.v_m_per_s[i] * \
-                                                                        self.output_area_m2[i]
-            self.mass_flow_kg_per_s.append(self.net.sink.loc[self.leakage_index[i], "mdot_kg_per_s"])
+        self.net.sink.loc[self.leakage_index, "mdot_kg_per_s"] = self.rho_kg_per_m3 * self.v_m_per_s * \
+                                                                 numpy.array(self.output_area_m2)
+        self.mass_flow_kg_per_s = self.net.sink.loc[self.leakage_index, "mdot_kg_per_s"]
 
         self.mass_flow_kg_per_s_init = self.mass_flow_kg_per_s
+
+    def finalize_control(self):
+        """
+        Delete the sinks that represented the leaks.
+        """
+        self.net.sink = self.net.sink.drop(labels=self.leakage_index)
+        self.net.res_sink = self.net.res_sink.drop(labels=self.leakage_index)
