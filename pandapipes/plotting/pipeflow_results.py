094e25c3069cfeb32bca81bf49bc9b6b1e00b192
lthurner <leon.thurner@iee.fraunhofer.de>
2021-01-28 16:59:04 +0100

    added test for pressure profile calculation

--- pandapipes/plotting/pipeflow_results.py
+++ pandapipes/plotting/pipeflow_results.py
@@ -20,7 +20,7 @@ def pressure_profile_to_junction_geodata(net):
         import pandapipes.networks as nw
         import pandapipes.plotting as plotting
         import pandapipes as pp
-    
+
         net = nw.schutterwald()
         pp.pipeflow(net)
         bgd = plotting.pressure_profile_to_junction_geodata(net)
@@ -31,16 +31,6 @@ def pressure_profile_to_junction_geodata(net):
 
     dist = top.calc_minimum_distance_to_junctions(net, net.ext_grid.junction.values)
     junctions = net.junction.index.values
-    bgd = pd.DataFrame({"x": dist.loc[junctions].values,
+    return pd.DataFrame({"x": dist.loc[junctions].values,
                         "y": net.res_junction.p_bar.loc[junctions].values},
                        index=junctions)
-    return bgd
-
-
-if __name__ == '__main__':
-    import pandapipes.networks as nw
-    import pandapipes as pp
-
-    net = nw.schutterwald()
-    pp.pipeflow(net)
-    bgd = pressure_profile_to_junction_geodata(net)
