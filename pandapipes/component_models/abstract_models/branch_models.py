ad3971d1b1ada584562839ae0e4de67495d88fd1
dlohmeier <daniel.lohmeier@iee.fraunhofer.de>
2021-01-29 12:36:56 +0100

    added options argument to lambda calculation call

--- pandapipes/component_models/abstract_models/branch_models.py
+++ pandapipes/component_models/abstract_models/branch_models.py
@@ -404,7 +404,8 @@ def calculate_derivatives_hydraulic(net, branch_pit, node_pit, options):
 
     dummy = (branch_pit[:, LENGTH] != 0).astype(np.float64)
     lambda_, re = calc_lambda(branch_pit[:, VINIT], branch_pit[:, ETA], branch_pit[:, RHO],
-                              branch_pit[:, D], branch_pit[:, K], gas_mode, friction_model, dummy)
+                              branch_pit[:, D], branch_pit[:, K], gas_mode, friction_model, dummy,
+                              options)
     der_lambda = calc_der_lambda(branch_pit[:, VINIT], branch_pit[:, ETA], branch_pit[:, RHO],
                                  branch_pit[:, D], branch_pit[:, K], friction_model, lambda_)
     branch_pit[:, RE] = re
