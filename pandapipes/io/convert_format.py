79c179ee06684ac04ea303d82cd4eabe08387572
sdrauz <Simon.Ruben.Drauz@iee.fraunhofer.de>
2022-10-21 14:44:43 +0200

    bugfix in hasattr

--- pandapipes/io/convert_format.py
+++ pandapipes/io/convert_format.py
@@ -23,7 +23,7 @@ def convert_format(net):
     format_version = version.parse(__format_version__)
     # For possible problems with this line of code, please check out
     # https://github.com/e2nIEE/pandapipes/issues/320
-    if not hasattr(net, __format_version__):
+    if not hasattr(net, 'format_version'):
         net.format_version = net.version
     if isinstance(net.format_version, str) and version.parse(net.format_version) >= format_version:
         return net
