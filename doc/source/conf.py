9636c29634b0ec0144cf1ccc570e8fd78de24fe7
sdrauz <Simon.Ruben.Drauz@iee.fraunhofer.de>
2023-01-03 14:53:19 +0100

    bugfix

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,7 +19,7 @@ sys.path.append(os.path.abspath('../../..'))
 sys.path.append(os.path.abspath('../../../pandapower'))
 
 
-master_doc = 'index'
+root_doc = 'index'
 # -- Project information -----------------------------------------------------
 
 project = 'pandapipes'
