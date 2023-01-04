9d805fa4f65ec7672679dd411c1d7e5648d4d949
sdrauz <Simon.Ruben.Drauz@iee.fraunhofer.de>
2023-01-04 21:40:03 +0100

    version tag update

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ long_description = '\n\n'.join((install, changelog))
 
 setup(
     name='pandapipes',
-    version='0.7.0',
+    version='0.8.0',
     author='Simon Ruben Drauz-Mauel, Daniel Lohmeier, Jolando Marius Kisse',
     author_email='simon.ruben.drauz@iee.fraunhofer.de, daniel.lohmeier@retoflow.de, '
                  'jolando.kisse@uni-kassel.de',
@@ -43,7 +43,7 @@ setup(
 	long_description_content_type='text/x-rst',
     url='http://www.pandapipes.org',
     license='BSD',
-    install_requires=["pandapower>=2.10.1", "matplotlib"],
+    install_requires=["pandapower>=2.11.1", "matplotlib"],
     extras_require={"docs": ["numpydoc", "sphinx", "sphinx_rtd_theme", "sphinxcontrib.bibtex"],
                     "plotting": ["plotly", "python-igraph"],
                     "test": ["pytest", "pytest-xdist", "nbmake"],
