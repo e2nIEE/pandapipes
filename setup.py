67e162abd44a16c226c3226b542a28b46750e916
sdrauz <Simon.Ruben.Drauz@iee.fraunhofer.de>
2023-01-05 15:13:26 +0100

    replace @ again

--- setup.py
+++ setup.py
@@ -34,16 +34,16 @@ long_description = '\n\n'.join((install, changelog))
 
 setup(
     name='pandapipes',
-    version='0.7.0',
+    version='0.8.0',
     author='Simon Ruben Drauz-Mauel, Daniel Lohmeier, Jolando Marius Kisse',
-    author_email='simon.ruben.drauz@iee.fraunhofer.de, daniel.lohmeier@retoflow.de, '
+    author_email='simon.ruben.drauz-mauel@iee.fraunhofer.de, daniel.lohmeier@retoflow.de, '
                  'jolando.kisse@uni-kassel.de',
     description='A pipeflow calculation tool that complements pandapower in the simulation of multi energy grids',
     long_description=long_description,
 	long_description_content_type='text/x-rst',
     url='http://www.pandapipes.org',
     license='BSD',
-    install_requires=["pandapower>=2.10.1", "matplotlib"],
+    install_requires=["pandapower>=2.11.1", "matplotlib"],
     extras_require={"docs": ["numpydoc", "sphinx", "sphinx_rtd_theme", "sphinxcontrib.bibtex"],
                     "plotting": ["plotly", "python-igraph"],
                     "test": ["pytest", "pytest-xdist", "nbmake"],
