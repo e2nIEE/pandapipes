cbc609893ff4a71103c5ba724f8b3325c5e45904
dlohmeier <daniel.lohmeier@retoflow.de>
2023-05-11 11:16:59 +0200

    added init file to controller package

---
+++ pandapipes/control/controller/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2020-2023 by Fraunhofer Institute for Energy Economics
+# and Energy System Technology (IEE), Kassel, and University of Kassel. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+from pandapipes.control.controller.pid_controller import PidControl
+from pandapipes.control.controller.differential_control import DifferentialControl
+from pandapipes.control.controller.collecting_controller import CollectorController
+from pandapipes.control.controller.logic_control import LogicControl
