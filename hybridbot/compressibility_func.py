575fbed5d2117c33b27e7484cbbda7887ec150a4
Khalil Ben Safta <khalil.ben.safta@iee.fraunhofer.de>
2023-06-07 11:57:47 +0200

    first vectorization trial

--- hybridbot/compressibility_func.py
+++ hybridbot/compressibility_func.py
@@ -3,8 +3,10 @@ from scipy.optimize import newton
 
 
 #ToDo: 1. Make function work; 2. Put Parameters in arrays; 3. Make function work for n_nodes>1(2-dimensional array)
-T = 283.15
-p = 50*1e5
+T = np.array([283.15, 283.15])
+#T = 283.15
+p = np.array([50*1e5, 45*1e5])
+#p = 50*1e5
 p_n = 101325
 T_n = 273.15
 # methane
@@ -23,25 +25,23 @@ p_crit_h2 = 13e5
 # acentric factor - dimensionless
 acent_fact_h2 = -0.219
 
-
 T_crit = np.array([T_crit_meth, T_crit_h2])
 p_crit = np.array([p_crit_meth, p_crit_h2])
 acent_fact = np.array([acent_fact_meth, acent_fact_h2])
 
-
-
 # Zum Testen'
-molar_fraction = np.array([0.11, 0.89])
+#molar_fraction = np.array([0.11, 0.89])
 
 # Ziel
-molar_fraction_real = np.array([[0.11, 0.89], [0.6, 0.4]])
+molar_fraction = np.array([[0.11, 0.89], [0.6, 0.4]])
 
+shape = np.shape(molar_fraction)
 
 def _calculate_A_B(a_p, a_T, a_molar_fraction,  a_p_crit, a_T_crit, a_acent_fact):
     """
     calculate initially for one node:
 
-    a_molar_fraction: 1d_array
+    a_molar_fraction: x-d_array
     p_crit : 1d-array
     T_crit : 1d-array
     a_acent_fact : 1d-array
@@ -59,15 +59,29 @@ def _calculate_A_B(a_p, a_T, a_molar_fraction,  a_p_crit, a_T_crit, a_acent_fact
 
     # def A_mixture(self, a_p, a_T, m, a_molar_fraction : np.ndarray, t_crit: np.ndarray, p_crit: np.ndarray):
     factor_a = 0.42747 * a_p / (a_T ** 2)
-    sum_a = (a_molar_fraction * a_T_crit * sqrt_alpha / a_p_crit ** 0.5).sum() ** 2
-    A_mixture = factor_a * sum_a
 
-    # B_mixture(self, a_p, a_T, a_molar_fraction):
-    factor_b = 0.08664 * a_p / a_T
-    sum_b = (a_molar_fraction * a_T_crit / a_p_crit).sum()
-    B_mixture = factor_b * sum_b
+    if len(shape) == 1:
+        sum_a = (a_molar_fraction * a_T_crit * sqrt_alpha / a_p_crit ** 0.5).sum() ** 2
+        A_mixture = factor_a * sum_a
 
-    return A_mixture, B_mixture
+        # B_mixture(self, a_p, a_T, a_molar_fraction):
+        factor_b = 0.08664 * a_p / a_T
+        sum_b = (a_molar_fraction * a_T_crit / a_p_crit).sum()
+        B_mixture = factor_b * sum_b
+
+        return A_mixture, B_mixture
+    else:
+        sum_a = (a_molar_fraction * a_T_crit * sqrt_alpha / a_p_crit ** 0.5).sum(axis=1) ** 2
+        A_mixture = factor_a * sum_a
+
+        # B_mixture(self, a_p, a_T, a_molar_fraction):
+        factor_b = 0.08664 * a_p / a_T
+        sum_b = (a_molar_fraction * a_T_crit / a_p_crit).sum(axis=1)
+        B_mixture = factor_b * sum_b
+
+        return A_mixture, B_mixture
+
+
 
 def _func_of_Z(Z, a_p, a_T):
     A_mixture, B_mixture = _calculate_A_B(a_p, a_T, molar_fraction, p_crit, T_crit, acent_fact)
@@ -78,11 +92,10 @@ def _der_func_of_Z(Z, a_p, a_T):
     return  3 * Z **2 - 2 * Z + (A_mixture - B_mixture - B_mixture **2)
 
 def calculate_mixture_compressibility_draft():
-    res_comp = newton(_func_of_Z, 0.9, fprime=_der_func_of_Z, args=(p, T))
+    res_comp = newton(_func_of_Z, np.array([0.9, .9]), fprime=_der_func_of_Z, args=(p, T))
     res_comp_norm = newton(_func_of_Z, 0.9, fprime=_der_func_of_Z, args=(p_n, T_n))
     res_comp / res_comp_norm
     return res_comp, res_comp_norm
-
 
 
 compr_mixture, compr_mixture_norm = calculate_mixture_compressibility_draft()
