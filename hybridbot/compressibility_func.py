d285f6d5bdc1a303e33aa941d07dbf23fb711ad4
eprade <erik.prade@iee.fraunhofer.de>
2023-06-12 14:14:06 +0200

    further changes for comp - array calculation

--- hybridbot/compressibility_func.py
+++ hybridbot/compressibility_func.py
@@ -57,24 +57,25 @@ def _calculate_A_B(a_p, a_T, a_molar_fraction,  a_p_crit, a_T_crit, a_acent_fact
     # shape = np.shape(a_molar_fraction)
     # com_array = np.empty([shape[0], 3], dtype=np.float64)
     shape = np.shape(a_p)
-    T_red = np.zeros((len(a_p_crit)*len(a_p)))
-    p_red = np.zeros((len(a_p_crit) * len(a_p)))
-    fluid_node_combination = 0
+    fluid_length = 2
+    T_red = np.zeros([len(a_p),len(a_p_crit)])
+    p_red = np.zeros([len(a_p), len(a_p_crit)])
+
+
 
     for node in range(len(a_p)):
-        T_red[fluid_node_combination] = a_T[node] / a_T_crit[0]
-        T_red[fluid_node_combination+1] = a_T[node] / a_T_crit[1]
-        p_red[fluid_node_combination] = a_p[node] / a_p_crit[0]
-        p_red[fluid_node_combination+1] = a_p[node] / a_p_crit[1]
-        fluid_node_combination += 2
+        T_red[node][0] = a_T[node] / a_T_crit[0]
+        T_red[node][1] = a_T[node] / a_T_crit[1]
+        p_red[node][0] = a_p[node] / a_p_crit[0]
+        p_red[node][1] = a_p[node] / a_p_crit[1]
 
 
 
-    m = 0.480 + 1.574 * a_acent_fact - 0.176 * a_acent_fact**2
-    m = list(m)
 
-    m = m * shape[0]
-    m = np.array(m)
+    m_val = 0.480 + 1.574 * a_acent_fact - 0.176 * a_acent_fact**2
+    m = m_val
+    for node in range(shape[0]-1): m = np.vstack([m, m_val])
+
     sqrt_alpha = (1 + m * (1 - T_red ** 0.5))
 
     # def A_mixture(self, a_p, a_T, m, a_molar_fraction : np.ndarray, t_crit: np.ndarray, p_crit: np.ndarray):
@@ -114,34 +115,16 @@ def _der_func_of_Z(Z, a_p, a_T, a_mf):
 def calculate_mixture_compressibility_draft(mf, p, T):
     shape = np.shape(mf)
     T = np.array([283.15, 283.15, 283.15])
+    start_value = 0.9
+    start_value_vec = np.array([list([start_value]) * shape[0]])[0]
+
 
     p_n = 101325
     T_n = 273.15
-    # methane
-    # critical temperature [K]
-    T_crit_meth = 190.55
-    # critical pressure [Pa]
-    p_crit_meth = 45.95e5
-    # acentric factor - dimensionless
-    acent_fact_meth = 0.01142
 
-    # h2
-    # critical temperature [K]
-    T_crit_h2 = 33.20
-    # critical pressure [Pa]
-    p_crit_h2 = 13e5
-    # acentric factor - dimensionless
-    acent_fact_h2 = -0.219
-
-    #T_crit = np.array([T_crit_meth, T_crit_h2])
-    T_crit = np.full([shape[0],2], np.array([T_crit_meth, T_crit_h2]))
-    #p_crit = np.array([p_crit_meth, p_crit_h2])
-    p_crit = np.full([shape[0], 2], np.array([p_crit_meth, p_crit_h2]))
-    #acent_fact = np.array([acent_fact_meth, acent_fact_h2])
-    acent_fact = np.full([shape[0], 2],np.array([acent_fact_meth, acent_fact_h2]))
-
-    res_comp = newton(_func_of_Z, np.array([0.9, 0.9]), fprime=_der_func_of_Z, args=(p, T, mf))
-    res_comp_norm = newton(_func_of_Z, np.array([0.9, 0.9]), fprime=_der_func_of_Z, args=(p_n, T_n))
+    res_comp = newton(_func_of_Z, start_value_vec, fprime=_der_func_of_Z, args=(p, T, mf))
+    # ToDo:check args of res_comp_norm (mf?)
+    res_comp_norm = newton(_func_of_Z, start_value_vec, fprime=_der_func_of_Z, args=(p_n, T_n, mf))
     res_comp / res_comp_norm
     return res_comp, res_comp_norm
 
