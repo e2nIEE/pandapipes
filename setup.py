0cbeaa90ecb294556369bd139d61e68b07d4afc0
sdrauz <Simon.Ruben.Drauz@iee.fraunhofer.de>
2023-12-13 14:06:01 +0100

    move everything in src folder

--- setup.py
+++ setup.py
@@ -49,7 +49,8 @@ setup(
                     "test": ["pytest", "pytest-xdist", "nbmake"],
                     "all": ["numpydoc", "sphinx", "sphinx_rtd_theme", "sphinxcontrib.bibtex",
                             "plotly", "igraph", "pytest", "pytest-xdist", "nbmake"]},
-    packages=['pandapipes.' + x for x in find_namespace_packages(where="pandapipes")]+ ['pandapipes'],
+    packages=find_namespace_packages(where='src'),
+    package_dir={"":"src"},
     include_package_data=True,
     classifiers=classifiers
 )
