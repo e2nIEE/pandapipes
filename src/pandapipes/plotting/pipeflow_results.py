db1ca1a5ea1a85275776f0082bb209dad5b74d58
Moritz Franz <moritz.franz@iee-extern.fraunhofer.de>
2024-03-25 15:37:49 +0100

    vectorized plotting

--- src/pandapipes/plotting/pipeflow_results.py
+++ src/pandapipes/plotting/pipeflow_results.py
@@ -7,6 +7,7 @@ import pandapipes.topology as top
 import pandas as pd
 import matplotlib.pyplot as plt
 from itertools import combinations
+import numpy as np
 
 
 def pressure_profile_to_junction_geodata(net):
@@ -42,7 +43,7 @@ def pressure_profile_to_junction_geodata(net):
 
 def plot_pressure_profile(net, ax=None, x0_junctions=None, plot_pressure_controller=True, xlabel="Distance from Slack in km",
                           ylabel="Pressure in bar", x0=0, pipe_color="tab:grey", pc_color="r",
-                          junction_colors="tab:blue", junction_size=3, pipes=None, **kwargs):
+                          junction_color="tab:blue", junction_size=3, pipes=None, **kwargs):
     """Plot the pressure profile depending on the distance from the x0_junction (slack).
 
     Parameters
@@ -67,7 +68,7 @@ def plot_pressure_profile(net, ax=None, x0_junctions=None, plot_pressure_control
         color used to plot the pipes, by default "tab:grey"
     pc_color : str, optional
         color used to plot the pressure controller, by default "r"
-    junction_colors : [str, dict[int, str]], optional
+    junction_color : [str, dict[int, str]], optional
         colors used to plot the junctions. Can be passed as string (to give all junctions the same color),
         or as dict, by default "tab:blue"
     junction_size : int, optional
@@ -93,43 +94,22 @@ def plot_pressure_profile(net, ax=None, x0_junctions=None, plot_pressure_control
 
     d = top.calc_distance_to_junctions(net, x0_junctions)
     pipe_table = net.pipe[net.pipe.in_service & net.pipe.index.isin(pipes)]
-    for from_junction, to_junction in zip(pipe_table.from_junction, pipe_table.to_junction):
-        if from_junction not in d.index:
-            continue
-        x = [x0 + d.at[from_junction], x0 + d.at[to_junction]]
-        try:
-            y = [net.res_junction.p_bar.at[from_junction], net.res_junction.p_bar.at[to_junction]]
-        except Exception as e:
-            raise UserWarning(e)  # todo: logger
-        if "linewidth" in kwargs:
-            ax.plot(x, y, color=pipe_color, **kwargs)
-        else:
-            ax.plot(x, y, linewidth=1, color=pipe_color, **kwargs)
-        if junction_colors is not None:
-            if isinstance(junction_colors, str):
-                junction_colors = {b: junction_colors for b in net.junction.index}
-            for junction, x, y in zip((from_junction, to_junction), x, y):
-                if junction in junction_colors:
-                    ax.plot(x, y, 'o', color=junction_colors[junction], ms=junction_size)
-        kwargs = {k: v for k, v in kwargs.items() if not k == "label"}
 
-    # pressure controller geodata
-    if plot_pressure_controller:
-        pc_table = "press_control"
-        if pc_table in net.keys():
-            pressure_controller = net[pc_table].query('in_service')
-            for pcid, pc in pressure_controller.iterrows():
-                pc_junctions = [pc[b_col] for b_col in ('from_junction', 'to_junction') if b_col in pc.index]
-                if any([b not in d.index.values or b not in net.res_junction.index.values for b in pc_junctions]):
-                    print('cannot add press_control %d to plot' % pcid)  # todo: logger
-                    #logger.info('cannot add press_control %d to plot' % pcid)
-                    continue
+    x = np.array([d.loc[pipe_table.from_junction].values, d.loc[pipe_table.to_junction].values]) + x0
+    y = np.array([net.res_junction.p_bar.loc[pipe_table.from_junction].values, net.res_junction.p_bar.loc[pipe_table.to_junction].values])
+    linewidth = kwargs.get("linewidth", 1)
+    ax.plot(x, y, linewidth=linewidth, color=pipe_color, **kwargs)
 
-                for bi, bj in combinations(pc_junctions, 2):
-                    pc_coords = ([x0 + d.loc[bi], x0 + d.loc[bj]],
-                                 [net.res_junction.at[bi, 'p_bar'], net.res_junction.at[bj, 'p_bar']])
-                    ax.plot(*pc_coords, color=pc_color,
-                            **{k: v for k, v in kwargs.items() if not k == "color"})
+    x = d.values
+    y = net.res_junction.p_bar.loc[d.index]
+    ax.plot(x, y, 'o', color=junction_color, ms=junction_size)
+
+    if plot_pressure_controller and ("press_control" in net.keys()):
+        pressure_controller = net.press_control.query('in_service')
+        x = np.array([d.loc[pressure_controller.from_junction].values, d.loc[pressure_controller.to_junction].values]) + x0
+        y = np.array([net.res_junction.p_bar.loc[pressure_controller.from_junction].values, net.res_junction.p_bar.loc[pressure_controller.to_junction].values])
+        ax.plot(x, y, color=pc_color, **{k: v for k, v in kwargs.items() if not k == "color"})
+
     if xlabel is not None:
         ax.set_xlabel(xlabel)
     if ylabel is not None:
