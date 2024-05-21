8f998fb5302ef5972865be7273e57e21b9a68371
EPrade <90843651+EPrade@users.noreply.github.com>
2024-05-21 11:38:37 +0200

    Update __init__.py
    changed version

--- src/pandapipes/__init__.py
+++ src/pandapipes/__init__.py
@@ -2,7 +2,7 @@
 # and Energy System Technology (IEE), Kassel, and University of Kassel. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-__version__ = '0.10.0'
+__version__ = '0.10.1'
 __format_version__ = '0.10.1'
 
 import pandas as pd
