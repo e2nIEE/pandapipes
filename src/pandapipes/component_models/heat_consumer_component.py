c8b834da9aa90a396bb335cd4f98c383193e4e90
Daniel Lohmeier <31214121+dlohmeier@users.noreply.github.com>
2024-11-07 11:34:57 +0100

    use np.any in heat_consumer_component.py

--- src/pandapipes/component_models/heat_consumer_component.py
+++ src/pandapipes/component_models/heat_consumer_component.py
@@ -78,8 +78,8 @@ class HeatConsumer(BranchWZeroLengthComponent):
         treturn = net[cls.table_name()].treturn_k.values
         hc_pit[~np.isnan(treturn), TOUTINIT] = treturn[~np.isnan(treturn)]
         mask_q0 = hc_pit[:, QEXT] == 0 & np.isnan(hc_pit[:, MDOTINIT])
-        hc_pit[mask_q0, ACTIVE] = False
-        if any(mask_q0):
+        if np.any(mask_q0):
+            hc_pit[mask_q0, ACTIVE] = False
             logger.warning(r'qext_w is equals to zero for heat consumers with index %s. '
                            r'Therefore, the defined temperature control cannot be maintained.' \
                     %net[cls.table_name()].index[mask_q0])
