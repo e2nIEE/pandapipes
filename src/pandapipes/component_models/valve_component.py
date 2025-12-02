b75094f146328239fd3f8ba73dcb285993d16613
srdm <simon.drauz-mauel@retoflow.de>
2025-12-02 22:59:35 +0100

    fix/valve_pipe

--- src/pandapipes/component_models/valve_component.py
+++ src/pandapipes/component_models/valve_component.py
@@ -111,8 +111,10 @@ class Valve(BranchWInternalsComponent):
             fn_col, tn_col = cls.from_to_node_cols()
             junction_idx_lookup = get_lookup(net, "node", "index")[Junction.table_name()]
             from_nodes = junction_idx_lookup[net[cls.table_name()][fn_col].values]
-            to_elements = net[cls.table_name()][tn_col].values
-            to_nodes = junction_idx_lookup[to_elements]
+            to_nodes = np.zeros_like(from_nodes, dtype=int)
+            mask_j = net[cls.table_name()].et == 'ju'
+            to_elements = net[cls.table_name()][tn_col]
+            to_nodes[mask_j] = junction_idx_lookup[to_elements[mask_j]]
             has_internals = np.any(internal_node_number > 0)
             if has_internals:
                 f, t = get_lookup(net, "branch", "from_to")['pipe']
